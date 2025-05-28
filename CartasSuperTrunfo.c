df814e6e590fe457c573ff8a6038dacb9d42423d
Luis8523 <luizinhogustavo999@gmail.com>
2025-05-28 17:33:29 -0300

    Update CartasSuperTrunfo.c
    intermediario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,9 @@
-
-
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -17,10 +13,8 @@ int main() {
     int populacao1, populacao2;
     float area1, area2, pib1, pib2;
     int pontos_turisticos1, pontos_turisticos2;
-
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, população, área, etc.
+    float densidade_populacional1, densidade_populacional2;
+    float pib_por_capita1, pib_por_capita2;
 
     // Cadastro da primeira carta
     printf("Digite o código da primeira carta (ex: A01): ");
@@ -54,19 +48,22 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos2);
 
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Cálculo da densidade populacional e PIB per capita
+    densidade_populacional1 = populacao1 / area1;
+    densidade_populacional2 = populacao2 / area2;
+    pib_por_capita1 = pib1 / populacao1;
+    pib_por_capita2 = pib2 / populacao2;
 
+    // Exibição dos Dados das Cartas
     printf("\nDados das Cartas Cadastradas:\n");
 
     // Exibição da primeira carta
-    printf("\nCarta %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n",
-           codigo1, populacao1, area1, pib1, pontos_turisticos1);
+    printf("\nCarta %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\nDensidade populacional: %.2f hab/km²\nPIB per capita: %.2f reais\n",
+           codigo1, populacao1, area1, pib1, pontos_turisticos1, densidade_populacional1, pib_por_capita1);
 
     // Exibição da segunda carta       
-    printf("\nCarta %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n",
-           codigo2, populacao2, area2, pib2, pontos_turisticos2);
+    printf("\nCarta %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\nDensidade populacional: %.2f hab/km²\nPIB per capita: %.2f reais\n",
+           codigo2, populacao2, area2, pib2, pontos_turisticos2, densidade_populacional2, pib_por_capita2);
 
     return 0;
 }
